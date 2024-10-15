d2162bbbb647ce6e390ff96c7289728ded2fc392
Ash digit <ashdigit44@gmail.com>
2024-10-15 19:41:51 +0100

    added useContext for user

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,26 @@ import Article from "./components/Article";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Header";
 import Comments from "./components/Comments";
-
+import { createContext, useState } from "react";
 import { Route } from "react-router-dom";
-
+export const loginContext = createContext();
 function App() {
+  const user = { username: "ash_digit", loggedIn: true };
+  //const [loggedIn, setLoggedIn] = useState(user);
   return (
     <>
-      <Header />
-      <Dashboard />
+      <loginContext.Provider value={user}>
+        <Header />
+        <Dashboard />
 
-      <Route path="/articles/">
-        <Articles />
-      </Route>
+        <Route path="/articles/">
+          <Articles />
+        </Route>
 
-      <Route path="/article/:article_id">
-        <Article />
-      </Route>
-
-      <Route path="/article/:article_id/comments">
-        <h2>Comments:</h2>
-        <Comments />
-      </Route>
+        <Route path="/article/:article_id">
+          <Article />
+        </Route>
+      </loginContext.Provider>
     </>
   );
 }
