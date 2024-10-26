e4c0f79cd06d47aead9a7554fb7bdea46613252c
Ash digit <ashdigit44@gmail.com>
2024-10-26 18:30:44 +0100

    no need to refresh the current tab after deleting a comment

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,26 +1,31 @@
 import { api } from "../utils/api";
 import { useState, useEffect, useContext } from "react";
 import { loginContext } from "../App";
-const deleteComment = (id) => {
-  api(`/comments/${id}`, "DELETE")
-    .then((response) => {
-      console.log(`comment with ID: ${id} is deleted.`);
-      console.log(response);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
+
 const Comments = ({ article_id }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [comments, setComments] = useState([]);
+  const [commentDeleted, setCommentDeleted] = useState(false);
   const user = useContext(loginContext);
+
+  const deleteComment = (id) => {
+    api(`/comments/${id}`, "DELETE")
+      .then((response) => {
+        setCommentDeleted(true);
+        console.log(`comment with ID: ${id} is deleted.`);
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   useEffect(() => {
     api(`/articles/${article_id}/comments`, "GET", null).then((comments) => {
       setComments(comments);
       setIsLoading(false);
     });
-  }, []);
+  }, [commentDeleted]);
   return (
     <>
       {isLoading ? (
